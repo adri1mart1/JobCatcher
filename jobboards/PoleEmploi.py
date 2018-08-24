f3eb4314e602bfb90dcd21e103e1386a01052d06
Adrien MARTIN <adrien.martin@martegy.net>
2018-08-25 00:38:53 +0200

    Pole emploi updated with new website version

--- jobboards/PoleEmploi.py
+++ jobboards/PoleEmploi.py
@@ -10,6 +10,7 @@ __version__ = '0.1'
 # System
 import re
 import time
+import html2text
 import glob
 from datetime import datetime
 
@@ -22,6 +23,8 @@ import utilities
 from jc.data import Offer
 from jc.jobboard import JobBoard
 
+# newly added
+import dateparser
 
 class JBPoleEmploi(JobBoard):
 
@@ -38,6 +41,7 @@ class JBPoleEmploi(JobBoard):
 
         for feed in glob.glob(searchdir):
             # Load the HTML feed
+
             page = utilities.openPage(feed)
 
             if page:
@@ -46,16 +50,15 @@ class JBPoleEmploi(JobBoard):
 
                 # Search result
                 res = re.finditer(
-                    r'<tr.*?>(.*?)</tr>',
+                    r'li data-id-offre="(.*?)"',
                     html,
                     flags=re.MULTILINE | re.DOTALL
                 )
+
                 for r in res:
-                    # Check if URL is valid
-                    m = re.search(r'href="\./resultats\.tableauresultatrechercheoffre:detailOffre/(.*?)"', r.group(1))
-                    if m:
-                        url = "http://candidat.pole-emploi.fr/candidat/rechercheoffres/detail/%s" % m.group(1)
-                        urls.append([feedid, url])
+                    string = r'/offres/recherche/detail/'+r.group(1)
+                    url = "https://candidat.pole-emploi.fr%s" % string
+                    urls.append([feedid, url])
 
         return urls
 
@@ -83,78 +86,134 @@ class JBPoleEmploi(JobBoard):
 
         return offerid
 
+    def _get_data_contract(self, soup):
+        """Get the contract type in text blop"""
+        res = None
+        desc_aside_item = soup.find('div', attrs={'class': 'description-aside col-sm-4 col-md-5'})
+        if desc_aside_item:
+            pattern_cdi = r'ind\xe9termin\xe9e'
+            pattern_cdd = r'd\xe9termin\xe9e'
+            pattern_interim = r'int\xe9rimaire'
+            html = unicode.join(u'\n', map(unicode, soup))
+
+            m = re.search(pattern_cdi, html, flags=re.MULTILINE | re.DOTALL)
+            if m:
+                return "CDI"
+
+            m = re.search(pattern_cdd, html, flags=re.MULTILINE | re.DOTALL)
+            if m:
+                return "CDD"
+
+            m = re.search(pattern_interim, html, flags=re.MULTILINE | re.DOTALL)
+            if m:
+                return "Interim"
+        return "Unknown"
+
+    def _get_title(self, soup):
+        """Get the job title"""
+        h2 = soup.find('h2', attrs={'class': 't2 title'})
+        if not h2:
+            return "Title not found"
+        return utilities.htmltotext(h2.text).strip()
+
+    def _get_published_date(self, soup):
+        """Getting the published date"""
+        date_item = soup.find('p', attrs={'class': 't5 title-complementary'})
+        if date_item:
+            m = re.search('[0-9]{1,2} \D* [0-9]{4}', str(date_item.text))
+            if m:
+                return int(time.mktime(dateparser.parse(m.group(0)).date().timetuple()))
+        return int(time.time())
+
+    def _get_company(self, soup):
+        company_item = soup.find('h4', attrs={'class': 't4 title'})
+        if not company_item:
+            return "N/A"
+        return utilities.htmltotext(company_item.text).strip()
+
+    def _get_department_num(self, soup):
+        """ Getting department number """
+        dep_num_item = soup.find('p', attrs={'class': 't4 title-complementary'})
+        if dep_num_item:
+            m = re.search('[0-9]{1,2}', utilities.htmltotext(dep_num_item.text).strip())
+            if m:
+                return m.group(0)
+        return None
+
+    def _get_location(self, soup):
+        """ Getting location (city) """
+        location_item = soup.find('p', attrs={'class': 't4 title-complementary'})
+        if location_item:
+            m = re.search(' - (.*) -', utilities.htmltotext(location_item.text).strip())
+            if m:
+                return m.group(1)
+        return "N/A"
+
     def analyzePage(self, page):
         """Analyze page and extract datas"""
 
-        if not self.requireAnalyse(page):
-            return ""
+        #if not self.requireAnalyse(page):
+        #    return ""
 
         self.datas['offerid'] = self.extractOfferId(page)
         soup = BeautifulSoup(page.content, fromEncoding=self.encoding['page'])
-        item = soup.body.find('div', attrs={'class': 'block-content'})
 
+        unavailable_offer = soup.body.find('Offre_indisponible')
+        if unavailable_offer:
+            self.disableOffer(self.datas['offerid'])
+            return "Offer not available"
+
+
+        # searching for modal-details modal-details-offre
+        item = soup.body.find('div', attrs={'class': 'modal-details modal-details-offre'})
         if not item:
             self.disableOffer(self.datas['offerid'])
-            return "Not block content found"
+            return "No modal-details-offre content found"
+
 
         # Title
-        h4 = item.find('h4', attrs={'itemprop': 'title'})
-        if not h4:
-            return "Title not found"
+        self.datas['title'] = self._get_title(item)
 
-        # Title & Url
-        self.datas['title'] = utilities.htmltotext(h4.text).strip()
+        # Url
         self.datas['url'] = page.url
 
         # Ref
+        # self.datas['ref'] = self._regexExtract(u'Numéro de l\'offre', li)
+        self.datas['ref'] = "N/A"
+
+        # Last update
         li = item.find('li', attrs={'class': 'primary'})
         self.datas['lastupdate'] = page.lastupdate
 
-        self.datas['ref'] = self._regexExtract(u'Numéro de l\'offre', li)
+        # Feed id
         self.datas['feedid'] = page.feedid
 
-        li = item.find('li', attrs={'class': 'secondary'})
-        if not li:
-            return "No seconday section found"
+        # Date of publication
+        self.datas['date_pub'] = self._get_published_date(item)
 
-        # Date
+        # Add in db date
         self.datas['date_add'] = int(time.time())
-        self.datas['date_pub'] = datetime.strptime(
-            self._regexExtract(u'Offre actualisée le', li),
-            "%d/%m/%Y").strftime('%s')
 
         # Job informations
-        self.datas['contract'] = self._regexExtract(
-            u'Type de contrat', item
-        )
+        self.datas['contract'] = self._get_data_contract(item)
 
         # Salary
-        self.datas['salary'] = self._regexExtract(
-            u'Salaire indicatif', item
-        )
+        self.datas['salary'] = "N/A"
+        # self.datas['salary'] = self._regexExtract(
+        #     u'Salaire indicatif', item
+        # )
         self.filterSalaries(self.datas)
 
         # Location
-        li = item.find('li', attrs={'itemprop': 'addressRegion'})
-        if not li:
-            return "No region section found"
-
-        self.datas['department'] = None
-        self.datas['location'] = li.text.strip()
-        m = re.search('([0-9]+) - (.*)', self.datas['location'])
-        if m:
-            self.datas['department'] = m.group(1).strip()
-            self.datas['location'] = m.group(2).strip()
+        self.datas['department'] = self._get_department_num(item)
+        self.datas['location'] = self._get_location(item)
 
         # Company
-        p = item.find('p', attrs={'itemprop': 'hiringOrganization'})
-        if p:
-            self.datas['company'] = p.text.strip()
-        else:
-            self.datas['company'] = "NA"
+        self.datas['company'] = self._get_company(item)
 
         # Insert to jobboard table
         self.datas['state'] = 'ACTIVE'
+
         self.insertToJBTable()
 
         return None
