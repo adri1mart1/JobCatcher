6ba9cbffa593eb6a042ba3f736833796943d4c8d
Adrien MARTIN <adrien.martin@martegy.net>
2018-09-30 21:11:08 +0200

    Add contributor

--- jobcatcher.py
+++ jobcatcher.py
@@ -5,6 +5,7 @@ __authors__ = [
     'Yoann Sculo <yoann.sculo@gmail.com>',
     'Bruno Adelé <bruno@adele.im>',
     'Yankel Scialom <yankel.scialom@mail.com>'
+    'Adrien Martin <adrien.martin@martegy.net>'
 ]
 __copyright__ = 'Copyright (C) 2013 Yoann Sculo'
 __license__ = 'GPLv2'
